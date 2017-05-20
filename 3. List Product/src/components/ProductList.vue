40de327f5c86f95d8780db48f99aad0bbd9aae1d
bonbonpa <darksphin@gmail.com>
2017-05-20 21:54:12 +0700

    add productList.vue

---
+++ 3. List Product/src/components/ProductList.vue
@@ -0,0 +1,17 @@
+<template lang="html">
+  <h1 class="header">I'm vue.js component</h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style lang="css">
+.header{
+  font-size: 4rem;
+}
+</style>
