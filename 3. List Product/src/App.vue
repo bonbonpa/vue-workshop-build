20ff02ef810acfc1c2eb0052b327daffdf4535c4
bonbonpa <darksphin@gmail.com>
2017-05-20 21:59:11 +0700

    chap 3 update

--- 3. List Product/src/App.vue
+++ 3. List Product/src/App.vue
@@ -1,21 +1,13 @@
 <template>
-  <!-- when using a component in a template Vue.js expects the DOM element to be
-  lowercased and dasherized. e.g. ProductList -> product-list -->
  <product-list></product-list>
-  <!-- use the <product-list> component here! -->
 </template>
 
 <script>
-// import product list component
 import ProductList from './components/ProductList'
 
 export default {
   components: {
-    // With ES6 we can use the Object Literal Shorthand Syntax to save us some
-    // keystrokes
     ProductList
-    // add product list as a component
-
   }
 }
 </script>
